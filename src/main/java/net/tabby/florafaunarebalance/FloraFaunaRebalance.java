1874aa02b690fe75c1aecbbf67e19955d2be303f
TNTost <TNTcreeper10.ender@gmail.com>
2023-12-17 21:11:16 +0100

    Functional base model+texture files

--- src/main/java/net/tabby/florafaunarebalance/FloraFaunaRebalance.java
+++ src/main/java/net/tabby/florafaunarebalance/FloraFaunaRebalance.java
@@ -1,14 +1,6 @@
 package net.tabby.florafaunarebalance;
 
 import com.mojang.logging.LogUtils;
-import net.minecraft.client.Minecraft;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.Material;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -18,32 +10,33 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
 import net.tabby.florafaunarebalance.item.ModItems;
+import net.tabby.florafaunarebalance.util.ModItemProperties;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(FloraFaunaRebalance.MOD_ID)
 public class FloraFaunaRebalance
-{
-    public static final String MOD_ID = "florafaunarebalance";
+{ public static final String MOD_ID = "florafaunarebalance";
     private static final Logger LOGGER = LogUtils.getLogger();
+
     public FloraFaunaRebalance()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
         ModItems.register(modEventBus);
 
-        modEventBus.addListener(this::commonSetup);
-        MinecraftForge.EVENT_BUS.register(this);
+        modEventBus.addListener(this::Setup);
+        modEventBus.addListener(this::clientSetup);
 
+        MinecraftForge.EVENT_BUS.register(this);
         ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
 
-    private void commonSetup(final FMLCommonSetupEvent event) {
+    private void clientSetup(final FMLClientSetupEvent event) {
+        ModItemProperties.addCustomItemProperties();
+    }
+    private void Setup(final FMLCommonSetupEvent event) {
     }
 
 
--- src/main/java/net/tabby/florafaunarebalance/item/ModItems.java
+++ src/main/java/net/tabby/florafaunarebalance/item/ModItems.java
@@ -1,19 +1,18 @@
 package net.tabby.florafaunarebalance.item;
 
+import net.minecraft.world.item.BowItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegisterEvent;
 import net.minecraftforge.registries.RegistryObject;
 import net.tabby.florafaunarebalance.FloraFaunaRebalance;
 
 public class ModItems {
-    public static final DeferredRegister<Item> ITEMS =
-            DeferredRegister.create(ForgeRegistries.ITEMS, FloraFaunaRebalance.MOD_ID);
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, FloraFaunaRebalance.MOD_ID);
     public static final RegistryObject<Item> DARTCHUTE = ITEMS.register("dart_chute",
-            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_COMBAT)));
+            () -> new BowItem(new Item.Properties().tab(CreativeModeTab.TAB_COMBAT).durability(43)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
