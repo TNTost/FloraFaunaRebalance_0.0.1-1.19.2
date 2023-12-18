a1813cd2339a55ebad63e1b7db0f9abf732c945b
TNTost <TNTcreeper10.ender@gmail.com>
2023-12-18 19:46:46 +0100

    revert

--- src/main/java/net/tabby/florafaunarebalance/item/custom/DartItem.java
+++ src/main/java/net/tabby/florafaunarebalance/item/custom/DartItem.java
@@ -3,14 +3,12 @@ package net.tabby.florafaunarebalance.item.custom;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.AbstractArrow;
+import net.minecraft.world.entity.projectile.Arrow;
 import net.minecraft.world.item.ArrowItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.level.Level;
-import net.tabby.florafaunarebalance.FloraFaunaRebalance;
-import net.tabby.florafaunarebalance.entity.custom.DartProjectileEntity;
-import net.tabby.florafaunarebalance.item.ModItemsINIT;
 
 public class DartItem extends ArrowItem {
     public final float damage;
@@ -20,7 +18,7 @@ public class DartItem extends ArrowItem {
     }
     @Override
     public AbstractArrow createArrow(Level p_40513_level, ItemStack p_40514_, LivingEntity p_40515_shooter) {
-        DartProjectileEntity arrow = new DartProjectileEntity(p_40515_shooter, p_40513_level, ModItemsINIT.DART.get());
+        Arrow arrow = new Arrow(p_40513_level, p_40515_shooter);
         arrow.setBaseDamage(this.damage);
         return arrow;
     }
