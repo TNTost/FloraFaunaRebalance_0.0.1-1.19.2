8c814c0c5550c8abcfcb16c43ccb5db32717ae5c
TNTost <TNTcreeper10.ender@gmail.com>
2023-12-24 00:13:50 +0100

    delete DartProjectileEntity

--- src/main/java/net/tabby/florafaunarebalance/entity/custom/DartProjectileEntity.java
+++ src/main/java/net/tabby/florafaunarebalance/entity/custom/DartProjectileEntity.java
@@ -3,6 +3,7 @@ package net.tabby.florafaunarebalance.entity.custom;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.projectile.AbstractArrow;
+import net.minecraft.world.entity.projectile.Arrow;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
@@ -10,20 +11,5 @@ import net.tabby.florafaunarebalance.entity.ModEntityType;
 import net.tabby.florafaunarebalance.item.ModItemsINIT;
 import org.jetbrains.annotations.NotNull;
 
-public class DartProjectileEntity extends AbstractArrow {
-    private final Item referenceItem;
-    public DartProjectileEntity(EntityType<? extends AbstractArrow> p_36721_type, Level p_36722_level) {
-        super(p_36721_type, p_36722_level);
-        this.referenceItem = ModItemsINIT.UNTIPPED_DART.get();
-    }
-
-    public DartProjectileEntity(LivingEntity p_36718_shooter, Level p_36719_level, Item referenceItem) {
-        super(ModEntityType.DART.get(), p_36718_shooter, p_36719_level);
-        this.referenceItem = referenceItem;
-    }
-
-    @Override
-    public @NotNull ItemStack getPickupItem() {
-        return new ItemStack(this.referenceItem);
-    }
+public class DartProjectileEntity extends Arrow {
 }
--- src/main/java/net/tabby/florafaunarebalance/item/custom/TippedDartItem.java
+++ src/main/java/net/tabby/florafaunarebalance/item/custom/TippedDartItem.java
@@ -5,18 +5,23 @@ import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.projectile.AbstractArrow;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.EntityHitResult;
+import net.tabby.florafaunarebalance.entity.custom.DartProjectileEntity;
+import net.tabby.florafaunarebalance.item.ModItemsINIT;
 
-public class TippedDartItem extends DartItem{
+public class TippedDartItem extends DartItem {
 public final MobEffect EFFECT;
     public TippedDartItem(Properties p_40512_properties, float damage, MobEffect effect) {
         super(p_40512_properties, damage);
          this.EFFECT = effect;
     }
-    protected void OnHitEntity(EntityHitResult entityHitResult){
-        Entity target = entityHitResult.getEntity();
-        if (target instanceof LivingEntity targetEntity) {
-            targetEntity.addEffect(new MobEffectInstance(this.EFFECT, 3, 3));
-        }
+    @Override
+    public AbstractArrow createArrow(Level p_40513_level, ItemStack p_40514_, LivingEntity p_40515_shooter) {
+        DartProjectileEntity arrow = new DartProjectileEntity(p_40515_shooter, p_40513_level, ModItemsINIT.POISON_DART.get());
+        arrow.setBaseDamage(this.DAMAGE);
+        return arrow;
     }
 }
