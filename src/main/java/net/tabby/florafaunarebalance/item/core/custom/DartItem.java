dd30ae4df7696350a74cadb3988fb9560ad7c46d
TabbyIshProto <TNTcreeper10.ender@gmail.com>
2024-01-25 18:52:45 +0100

    delete garbage infinity arrow check

--- src/main/java/net/tabby/florafaunarebalance/item/core/custom/DartItem.java
+++ src/main/java/net/tabby/florafaunarebalance/item/core/custom/DartItem.java
@@ -4,13 +4,9 @@ import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.ListTag;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.AbstractArrow;
 import net.minecraft.world.item.ArrowItem;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.alchemy.PotionUtils;
-import net.minecraft.world.item.enchantment.EnchantmentHelper;
-import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.level.Level;
 import net.tabby.florafaunarebalance.entity.custom.DartProjectileEntity;
 
@@ -44,10 +40,4 @@ public class DartItem extends ArrowItem {
         }
         return dart;
     }
-    @Override
-    public boolean isInfinite(ItemStack stack, ItemStack chute, Player player) {
-        int enchant = EnchantmentHelper.getItemEnchantmentLevel(Enchantments.INFINITY_ARROWS, chute);
-        return enchant > 0 && this.getClass() == DartItem.class; //# actually pretty useless
-    }
-
 }
