9dd56e1cd5cedc41aa3192df89629c28ce6841c8
TabbyIshProto <TNTcreeper10.ender@gmail.com>
2024-02-02 01:24:52 +0100

    multiple dart textures

--- src/main/java/net/tabby/florafaunarebalance/client/renderer/entity/DartProjectileRenderer.java
+++ src/main/java/net/tabby/florafaunarebalance/client/renderer/entity/DartProjectileRenderer.java
@@ -6,13 +6,16 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.tabby.florafaunarebalance.FloraFaunaRebalance;
+import net.tabby.florafaunarebalance.client.renderer.entity.core.DartVariants;
 import net.tabby.florafaunarebalance.entity.custom.DartProjectileEntity;
 import org.jetbrains.annotations.NotNull;
 
 @OnlyIn(Dist.CLIENT)
 public class DartProjectileRenderer extends ArrowRenderer<DartProjectileEntity> {
     private static final ResourceLocation DART_TEXTURE = new ResourceLocation(FloraFaunaRebalance.MOD_ID, "textures/entity/projectile/dart.png");
-    //# texture location.^^
+    private static final ResourceLocation POISON_DART_TEXTURE = new ResourceLocation(FloraFaunaRebalance.MOD_ID, "textures/entity/projectile/poison.png");
+    private static final ResourceLocation DART_OF_HEALING_TEXTURE = new ResourceLocation(FloraFaunaRebalance.MOD_ID, "textures/entity/projectile/healing.png");
+    //# texture locations.^^
 
     public DartProjectileRenderer(EntityRendererProvider.Context context) {
         super(context);
@@ -20,11 +23,11 @@ public class DartProjectileRenderer extends ArrowRenderer<DartProjectileEntity> 
 
     @Override
     public ResourceLocation getTextureLocation(DartProjectileEntity dartEntity) {
-        if (dartEntity.getVariant().equals("untipped_dart")) {
-            System.out.println("hellowo, you are using the interface correctly");
-            return DART_TEXTURE; //# decide here which texture to use...
-        } else {
-            return DART_TEXTURE;
-        }
+        return switch (dartEntity.getVariant()) { //# you can return a switch statement...
+            case "untipped_dart" -> DART_TEXTURE;
+            case "poison_dart" -> POISON_DART_TEXTURE;
+            case "dart_of_healing" -> DART_OF_HEALING_TEXTURE; //# choose between diff dart textures.
+            default -> DART_TEXTURE;
+        };
     }
 }
