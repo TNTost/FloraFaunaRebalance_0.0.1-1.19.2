27da9682fdbb42a1e4d11fb2c03801b6df60e379
TabbyIshProto <TNTcreeper10.ender@gmail.com>
2024-02-12 13:39:51 +0100

    replace player.getProjectile() with non convoluted method (it removes default to arrow)

--- src/main/java/net/tabby/florafaunarebalance/item/core/custom/ChuteItem.java
+++ src/main/java/net/tabby/florafaunarebalance/item/core/custom/ChuteItem.java
@@ -5,7 +5,6 @@ import com.mojang.math.Quaternion;
 import com.mojang.math.Vector3f;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.AbstractArrow;
@@ -15,16 +14,19 @@ import net.minecraft.world.item.*;
 import net.minecraft.world.level.Level;
 import net.tabby.florafaunarebalance.entity.custom.DartProjectileEntity;
 import net.tabby.florafaunarebalance.item.FFRii;
-import net.tabby.florafaunarebalance.util.FFRTags;
+import net.tabby.florafaunarebalance.util.all.FFRUtil;
 import org.jetbrains.annotations.NotNull;
 
 
 import java.util.function.Predicate;
 
+import static java.lang.StrictMath.sqrt;
 import static net.tabby.florafaunarebalance.util.FFRTags.Items.*;
+import static net.tabby.florafaunarebalance.util.all.FFRUtil.getAmmo;
+
 //# make it so ChuteItem extends ProjectileWeaponItem instead...
 public class ChuteItem extends ProjectileWeaponItem {
-    public static final float BREATH_DURATION_CAP = 14.0f;
+    public static final float BREATH_DURATION_CAP = 17.0f;
     public static final Predicate<ItemStack> DART_ONLY = itemStack -> itemStack.is(DART_TAG);
     public static final Predicate<ItemStack> DART_OR_FIREWORKS = DART_ONLY.or(itemStack -> itemStack.is(Items.FIREWORK_ROCKET));
 
@@ -48,28 +50,22 @@ public class ChuteItem extends ProjectileWeaponItem {
 
     public void releaseUsing(ItemStack chuteItem, Level level, LivingEntity entity, int t) {
         if (entity instanceof Player player) { //# instanceof <Type> "variableName" makes new var.
-            ItemStack ammo = player.getProjectile(chuteItem); //# loops through inv to find item.
+            ItemStack ammo = getProjectile(player, chuteItem, getAllSupportedProjectiles()); //# loops through inv to find item.
             boolean inf = player.getAbilities().instabuild;
 
             if (!ammo.isEmpty() || inf) {
-                float pow = getPowerForTime(t = getUseDuration(chuteItem) - t);
-                float fwPow = getPowerForTime(t / 3.0f);
-                boolean fwFullPow = !(fwPow < 0.9f) && ammo.is(Items.FIREWORK_ROCKET);
-                if (!fwFullPow && ammo.is(Items.FIREWORK_ROCKET)) {
-                    ammo = getAltProjectile(player, chuteItem, Items.FIREWORK_ROCKET);
-                } else if (fwFullPow) {
-                    pow = fwPow; // TODO: sloppy, doesnt consume correct item >:(
-                }
-                //float pow = (pow = getPowerForTime(t = getUseDuration(chuteItem) - t)) < 0.9f && !ammo.is(Items.FIREWORK_ROCKET) ? pow : getPowerForTime(t / 3.0f); //# calculate strength.
-                //System.out.println(pow);
-                //boolean fwFullPow = !( < 0.9f) && ammo.is(Items.FIREWORK_ROCKET);
                 if (ammo.isEmpty() || ammo.is(Items.ARROW)) { //# remove arrow...
                     ammo = new ItemStack(FFRii.DART.get()); //# set dart in case of no item present.
                 }
-                if ((double) pow >= 0.35) { //# TODO: fireworks calls getArrow despite not firing due to low power...
-                    Projectile projectile = fwFullPow ? new FireworkRocketEntity(level, ammo, player, player.getX(), player.getEyeY() - 0.15000000596046448, player.getZ(), true) : null; //# set as new firework rocket when ammo matches, otherwise null.
-                    System.out.println(projectile);
-                    shootProjectile(projectile, level, player, chuteItem, ammo, pow, inf); //# shoot the projectile, duh.
+                float pow = getPowerForTime(t = getUseDuration(chuteItem) - t);
+                if (ammo.is(Items.FIREWORK_ROCKET)) {
+                    pow = (pow = getPowerForTime(t / 1.6f)) >= 0.9f ? pow : 0.0f;
+                    ammo = getProjectile(player, chuteItem, itemStack -> itemStack.is(Items.FIREWORK_ROCKET)); //# predicate usage...
+                }
+                if (pow >= 0.15f) { //# TODO: fireworks calls getArrow despite not firing due to low power...
+                    //Projectile projectile = fwFullPow ? new FireworkRocketEntity(level, ammo, player, player.getX(), player.getEyeY() - 0.15000000596046448, player.getZ(), true) : null; //# set as new firework rocket when ammo matches, otherwise null.
+                    //System.out.println(projectile);
+                    shootProjectile(null, level, player, chuteItem, ammo, pow, inf); //# shoot the projectile, duh.
                     //level.playSound(null, player.getX(), player.getY(), player.getZ(), SoundEvents.LLAMA_SPIT, SoundSource.PLAYERS, 1.0f, (level.getRandom().nextFloat() - level.getRandom().nextFloat()) * 0.2f); //# interesting sound <- accidental creation
                     if (!inf) { //# if in creative, do-not test for fireworks here...
                         ammo.shrink(1);
@@ -82,23 +78,11 @@ public class ChuteItem extends ProjectileWeaponItem {
         }
     }
     public static float getPowerForTime(float ivt) { //# inverse of time
-        float str = ivt / (BREATH_DURATION_CAP);
-        str = Math.min(1.0f, (str * str + str * 2.0f) / 3.0f);
-        System.out.println(str);
-        return str;
+        return (ivt = (float) (1 - sqrt(1 - ivt / BREATH_DURATION_CAP))) >= 1.0f || (ivt != ivt) ? 1.0f : ivt; //# 1 minus sqrt of 1 minus x...
     }
-
-    public ItemStack getAltProjectile (Player player, ItemStack rangedWpn, Item excludedItem) {
-        Predicate<ItemStack> predicate = ((ProjectileWeaponItem) rangedWpn.getItem()).getAllSupportedProjectiles();
-        for (int idx= 0; idx < player.getInventory().getContainerSize(); ++idx) {
-            ItemStack ammo = player.getInventory().getItem(idx);
-            if (!ammo.is(excludedItem)) {
-                if (predicate.test(ammo)) {
-                    return net.minecraftforge.common.ForgeHooks.getProjectile(player, rangedWpn, ammo);
-                }
-            }
-        }
-        return ItemStack.EMPTY;
+    //# math was here, but redundant float > double.
+    public ItemStack getProjectile(Player player, ItemStack rangedWpn, Predicate<ItemStack> predicate) {
+        return net.minecraftforge.common.ForgeHooks.getProjectile(player, rangedWpn, FFRUtil.getAmmo(player, predicate));
     }
 
 
--- src/main/java/net/tabby/florafaunarebalance/util/all/FFRUtil.java
+++ src/main/java/net/tabby/florafaunarebalance/util/all/FFRUtil.java
@@ -1,12 +1,27 @@
 package net.tabby.florafaunarebalance.util.all;
 
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.registries.ForgeRegistries;
 import java.util.Objects;
+import java.util.function.Predicate;
 
 public class FFRUtil {
+    public static ItemStack getAmmo(Player ply, Predicate<ItemStack> predicate) {
+        for (int idx = 0; idx < ply.getInventory().getContainerSize(); ++idx) {
+            ItemStack ammo = ply.getInventory().getItem(idx);
+            if (predicate.test(ammo)) {
+                return ammo;
+            }
+        }
+        return ItemStack.EMPTY;
+    }
+
     public static String getRgStr(Item item) { //# get string from Item...
         return Nn(ForgeRegistries.ITEMS.getKey(item));
     }
