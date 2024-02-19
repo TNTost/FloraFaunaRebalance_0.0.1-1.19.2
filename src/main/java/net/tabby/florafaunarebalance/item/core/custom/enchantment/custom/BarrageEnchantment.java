1af92d057f265d1273a3fca2ac285f73b196f640
TabbyIshProto <TNTcreeper10.ender@gmail.com>
2024-02-19 06:56:12 +0100

    fail misurably

--- src/main/java/net/tabby/florafaunarebalance/item/core/custom/enchantment/custom/BarrageEnchantment.java
+++ src/main/java/net/tabby/florafaunarebalance/item/core/custom/enchantment/custom/BarrageEnchantment.java
@@ -1,10 +1,13 @@
 package net.tabby.florafaunarebalance.item.core.custom.enchantment.custom;
 
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentCategory;
+import org.jetbrains.annotations.NotNull;
 
 public class BarrageEnchantment extends Enchantment {
     public BarrageEnchantment(Rarity rarity, EnchantmentCategory category, EquipmentSlot... slots) {
@@ -13,6 +16,20 @@ public class BarrageEnchantment extends Enchantment {
     @Override
     public int getMaxLevel() {
         return 3;
+    }
+
+    @Override
+    public void doPostAttack(LivingEntity living, Entity entity, int i) {
+        super.doPostAttack(living, entity, i);
+        entity.invulnerableTime = 0;
+        living.invulnerableTime = 0;
+    }
+
+    @Override
+    public void doPostHurt(@NotNull LivingEntity living, @NotNull Entity entity, int i) {
+        super.doPostHurt(living, entity, i);
+        entity.invulnerableTime = 0;
+        living.invulnerableTime = 0;
     }
 
     @Override
