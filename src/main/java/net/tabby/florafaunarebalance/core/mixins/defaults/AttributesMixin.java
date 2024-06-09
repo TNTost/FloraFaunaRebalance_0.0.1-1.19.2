76fcfb5acb96842380387e019df1e11219c1b223
TabbyIshProto <TNTcreeper10.ender@gmail.com>
2024-06-09 10:54:41 +0200

    fix health mixin bug

--- src/main/java/net/tabby/florafaunarebalance/core/mixins/defaults/AttributesMixin.java
+++ src/main/java/net/tabby/florafaunarebalance/core/mixins/defaults/AttributesMixin.java
@@ -1,22 +1,32 @@
 package net.tabby.florafaunarebalance.core.mixins.defaults;
 
-import net.minecraft.core.Registry;
-import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.attributes.RangedAttribute;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(Attributes.class)
 public class AttributesMixin {
-    @Inject(method = "register", at = @At("HEAD"), cancellable = true)
-    private static void ffr$slowDefaultMiningSpeed(String str, Attribute atr, CallbackInfoReturnable<Attribute> cir) {
-        double health = 16.5;
-        if (str.equals("generic.max_health")) {
-            cir.setReturnValue(Registry.register(Registry.ATTRIBUTE, str, new RangedAttribute("attribute.name.generic.max_health", health, 1.0D, 1024.0D)).setSyncable(true));
-            System.out.printf("#####################&*=- %s reduced to %s internally as double -=*&#####################%n", str, health); // slowed -> reduced..
+
+
+    @Redirect(method = "<clinit>", at = @At(value = "NEW", target = "net.minecraft.world.entity.ai.attributes.RangedAttribute"))
+    private static RangedAttribute ffr$rangedAttributeRedirect(String translationKey, double fallback, double min, double max) {
+        double health = 16d;
+        if (translationKey.equals("attribute.name.generic.max_health")) {
+            System.out.printf("#####################&*=- %s reduced to %s internally as double -=*&#####################%n", translationKey, health); // slowed -> reduced..
+            return new RangedAttribute(translationKey, health, min, max);
+        } else {
+            return new RangedAttribute(translationKey, fallback, min, max);
         }
     }
+    //@Inject(method = "register", at = @At("HEAD"), cancellable = true)
+    //private static void ffr$slowDefaultMiningSpeed(String str, Attribute atr, CallbackInfoReturnable<Attribute> cir) {
+    //    double health = 16;
+    //    if (str.equals("generic.max_health")) {
+    //        cir.setReturnValue(Registry.register(Registry.ATTRIBUTE, str, new RangedAttribute("attribute.name.generic.max_health", health, 1.0D, 1024.0D)).setSyncable(true));
+//
+    //    }
+    //    //# TODO: implement yat's optimisation.
+    //}
 }
